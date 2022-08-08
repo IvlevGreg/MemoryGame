01bc493defed12b5f5d96a3a2f1542d952517824
IvlevGreg <95238714+IvlevGreg@users.noreply.github.com>
2022-08-08 15:02:09 +0300

    Revert "first load, problem with reload"
    This reverts commit e3f6040ca394efb699dcea4bc3b9820fe139f192.

--- 14_async-event-loop/js/main.js
+++
@@ -1,8 +0,0 @@
-import renderMainPage from './modules/render-main-page.js'
-
-const container = document.getElementById('list')
-  renderMainPage(['./create-all-episodes-list.js', 
-  'https://swapi.dev/api/films', 
-  'https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css',
-  './css/style.css'], 
-  container )
--- 14_async-event-loop/js/modules/prepare-element.js
+++
@@ -1,8 +0,0 @@
-
-export default function prepareElement(tag, classes, text = 0) {
-  const el = document.createElement(tag);
-  if(Array.isArray(classes)) {el.classList.add(...classes)}
-  else {el.classList.add(classes)}
-  if(text) {el.textContent = text}
-  return el
-}
