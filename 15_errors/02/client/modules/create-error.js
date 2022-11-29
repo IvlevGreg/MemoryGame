456ce37ff780db11f164525fcbb6dd3655fbb4f9
IvlevGreg <95238714+IvlevGreg@users.noreply.github.com>
2022-11-29 21:28:22 +0300

    import project gitlab to github

--- 15_errors/02/client/modules/create-error.js
+++
@@ -1,8 +0,0 @@
-export default function createError(text, errorContainer) {
-  const p = document.createElement('p')
-  p.classList.add('error__text')
-  p.textContent = text
-
-  errorContainer.append(p)
-  return p
-}
--- 19_build/src/modules.js
+++
@@ -1,1 +0,0 @@
-import './style/bootstrap.scss';
